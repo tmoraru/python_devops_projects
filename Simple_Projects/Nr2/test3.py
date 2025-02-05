192a0cec3024149f61016f9a4a717e67374ae661
tmoraru <tatianamoraru1998@gmail.com>
2025-02-04 22:25:27 -0600

    new sections

--- Simple_Projects/Nr2/test3.py
+++ Simple_Projects/Nr2/test3.py
@@ -2,5 +2,8 @@ str1 = "Hello"
 str2 = "World"
 
 result = str1 + " " + str2
+#print only Hellow World 
 print(result)
+#print HELLOW WORLD because is used inbuild funct - upper
+print(result.up)per()
 
